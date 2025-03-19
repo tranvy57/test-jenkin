4289aecbee15aca6e4cd8f7bf09c831234986bf0
zy <tranvy.art@gmail.com>
2025-03-19 15:58:03 +0700

    add api

--- src/main/java/com/example/demojenkin/TestController.java
+++ src/main/java/com/example/demojenkin/TestController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String test() {
-        return "Em Zy cute";
+        return "Em Zy cute vai lon";
+    }
+
+    @GetMapping ("/tesst")
+    public String add() {
+        return "add gi do";
     }
 }

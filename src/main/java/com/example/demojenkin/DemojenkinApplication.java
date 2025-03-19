1847ed5f52666c54f86b9a2e185340f974468106
zy <tranvy.art@gmail.com>
2025-03-19 15:43:03 +0700

    first commit

---
+++ src/main/java/com/example/demojenkin/DemojenkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojenkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemojenkinApplication.class, args);
+    }
+
+}

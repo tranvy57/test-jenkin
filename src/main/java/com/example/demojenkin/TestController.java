615d210a64dbbdbaa1e78face9d1b38f563e232e
zy <tranvy.art@gmail.com>
2025-04-22 19:21:08 +0700

    add readme

--- src/main/java/com/example/demojenkin/TestController.java
+++ src/main/java/com/example/demojenkin/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String test() {
-        return "Em Zy cute vai lon";
+        return "Test Jenkin";
     }
 
     @GetMapping ("/tesst")
